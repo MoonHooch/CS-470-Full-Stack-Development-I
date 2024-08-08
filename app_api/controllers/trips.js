3c4cfe8e6bd5ac00222081c6dde91efc9c7fc0b7
Christian Delegas <christian.delegas@gmail.com>
2024-08-08 19:09:21 -0400

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -41,7 +41,66 @@ const tripsFindByCode = async(req, res) => {
     } 
 };
 
+// POST a single trip
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.start,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+        if(!q)
+        { // Database returned no data
+            return res
+                .status(400)
+                .json(err);
+        } else { // Return new trip
+            return res
+                .status(201)
+                .json(q);
+        }
+}
+
+// EDIT a single trip
+const tripsUpdateTrip = async(req, res) => {
+    const q = await Model
+        .findOneAndUpdate(
+            { 'code' : req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+
+        if(!q)
+        { // Database returned no data
+            return res
+                .status(400)
+                .json(err);
+        } else { // Return resulting updated trip
+            return res
+                .status(201)
+                .json(q);
+        }
+};
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,10 +7,13 @@ const tripsController = require('../controllers/trips');
 // Define route for the trips endpoint
 router
     .route('/trips')
-    .get(tripsController.tripsList); // GET method routes tripsList
+    .get(tripsController.tripsList) // GET method routes tripsList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
+    
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode); // GET method routes
-
+    .get(tripsController.tripsFindByCode) // GET method routes
+    .put(tripsController.tripsUpdateTrip);
+    
 module.exports = router;
